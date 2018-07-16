849196dd182093a393558636df0b3974115b4b57
Vincent Bernardoff <vb@luminar.eu.org>
2018-07-16 22:26:28 +0200

    add `addr_of_uri` helper function

--- src/bmex_common.mli
+++ src/bmex_common.mli
@@ -1,5 +1,10 @@
 open Async
 module DTC = Dtc_pb.Dtcprotocol_piqi
 
+val addr_of_uri :
+  Uri.t -> Conduit_async.V2.addr Deferred.t
+(** [addr_of_uri uri] resolves [uri] and returns a value suitable for
+    use with [Conduit_async.V2]. *)
+
 val write_message :
   Writer.t -> DTC.dtcmessage_type -> ('a -> Piqirun.OBuf.t) -> 'a -> unit
