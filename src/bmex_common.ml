fb5dc7fee34a3c3a3fb621e0043d02bdd8ea1c7e
Vincent Bernardoff <vb@luminar.eu.org>
2019-05-19 13:01:04 +0200

    add span_of_interval

--- src/bmex_common.ml
+++ src/bmex_common.ml
@@ -3,6 +3,25 @@ open Async
 
 module DTC = Dtc_pb.Dtcprotocol_piqi
 
+let span_of_interval :
+  DTC.historical_data_interval_enum -> Time_ns.Span.t = function
+  | `interval_1_week -> Time_ns.Span.of_day 7.
+  | `interval_1_day -> Time_ns.Span.of_day 1.
+  | `interval_2_hours -> Time_ns.Span.of_int_sec (120 * 60)
+  | `interval_1_hour -> Time_ns.Span.of_int_sec (60 * 60)
+  | `interval_30_minute -> Time_ns.Span.of_int_sec (30 * 60)
+  | `interval_15_minute -> Time_ns.Span.of_int_sec (15 * 60)
+  | `interval_10_minute -> Time_ns.Span.of_int_sec (10 * 60)
+  | `interval_5_minute -> Time_ns.Span.of_int_sec (5 * 60)
+  | `interval_1_minute -> Time_ns.Span.of_int_sec 60
+  | `interval_30_seconds -> Time_ns.Span.of_int_sec 30
+  | `interval_10_seconds -> Time_ns.Span.of_int_sec 10
+  | `interval_5_seconds -> Time_ns.Span.of_int_sec 5
+  | `interval_4_seconds -> Time_ns.Span.of_int_sec 4
+  | `interval_2_seconds -> Time_ns.Span.of_int_sec 2
+  | `interval_1_second -> Time_ns.Span.of_int_sec 1
+  | `interval_tick -> Time_ns.Span.zero
+
 let write_message w (typ : DTC.dtcmessage_type) gen msg =
   let typ =
     Piqirun.(DTC.gen_dtcmessage_type typ |> to_string |> init_from_string |> int_of_varint) in
--- src/bmex_common.mli
+++ src/bmex_common.mli
@@ -1,5 +1,10 @@
+open Core
 open Async
+
 module DTC = Dtc_pb.Dtcprotocol_piqi
+
+val span_of_interval :
+  DTC.historical_data_interval_enum -> Time_ns.Span.t
 
 val write_message :
   Writer.t -> DTC.dtcmessage_type -> ('a -> Piqirun.OBuf.t) -> 'a -> unit
